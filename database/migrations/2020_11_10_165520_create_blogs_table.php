4d19f24a46cb6f1688c64e1acf7d4626f010ff9f
oniharnantyo_linkaja <oni_harnantyo@linkaja.com>
2022-04-01 08:49:17 +0700

    feat: added user and role seeder

--- database/migrations/2020_11_10_165520_create_blogs_table.php
+++ database/migrations/2020_11_10_165520_create_blogs_table.php
@@ -15,7 +15,7 @@ class CreateBlogsTable extends Migration
     {
         Schema::create('blogs', function (Blueprint $table) {
             $table->uuid('id')->unique()->primary();
-            $table->uuid('author_id');
+            $table->integer('author_id');
             $table->string('title');
             $table->string('thumbnail');
             $table->string('slug')->unique();
---
+++ database/seeders/CreateAdminUserSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Domain\User\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class CreateAdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('password')
+        ]);
+
+        $role = Role::create(['name' => 'admin']);
+
+        $permissions = Permission::pluck('id', 'id')->all();
+
+        $role->syncPermissions($permissions);
+
+        $user->assignRole([$role->id]);
+    }
+}
